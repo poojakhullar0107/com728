d34753c46e5d614f40d4aa5d83a0a2d0210f2805
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-02-12 11:35:41 +0000

    >,< or =

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,10 @@
+print("enter your first number")
+num1=int(input())
+print("enter your second number")
+num2=int(input())
+if num1>num2:
+    print ("first number is bigger")
+elif num2>num1:
+    print("second number is bigger")
+else:
+    print("both the enumbers are same")
