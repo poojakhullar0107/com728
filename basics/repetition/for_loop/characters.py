d42ff1e8b687e27c85410bc9866860d964808de0
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-02-16 13:56:54 +0000

    for loop

---
+++ basics/repetition/for_loop/characters.py
@@ -0,0 +1,7 @@
+print("What strange markings do you see?")
+string=input()
+print("identifying....")
+for i in range (0, len(string),1):
+    print(f"Index {i} is : {string[i]}")
+
+print("Done")
---
+++ basics/repetition/for_loop/range.py
@@ -0,0 +1,7 @@
+print ("What level of brightness is required?")
+b_level=int(input())
+print("Adjusting Brightness")
+for i in range (2,b_level+1,2):
+    print(f"Beeps brightness level   : {'*'* i}")
+    print(f"Bops brightness level is : {'*' * i}")
+print("Adjustments Completed..")
