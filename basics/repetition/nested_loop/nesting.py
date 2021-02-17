b02d8ebcb53171a640086d246c9c265987bfdddf
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-02-17 14:26:05 +0000

    nesting

--- basics/repetition/nested_loop/nesting.py
+++ basics/repetition/nested_loop/nesting.py
@@ -2,14 +2,12 @@ print("please enter a sequence")
 sequence=input()
 print("please input character for the marker")
 character=input()
-count=0
-sum=0
-for i in range (len(sequence)):
-    if (count==0) and (sequence[i]==character):
-        print("found first character")
-        count=1
-    elif count==1 and sequence[i]==character :
-        print ("found last character")
-    elif count==1:
-        sum+=1
-print(f"Distance between the marker is {sum}")
+p1=-1
+p2=-1
+for p in range(len(sequence)):
+    if sequence[p]==character:
+        if p1==-1:
+            p1=p
+        else:
+            p2=p
+print(f"Difference between marker is {p2-p1-1}")
