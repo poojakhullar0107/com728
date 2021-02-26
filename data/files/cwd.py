f37a8262e7394c959e8064a6e85b17709bf443fa
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-02-26 10:23:27 +0000

    if __name__ ==  __main__:

---
+++ data/files/cwd.py
@@ -0,0 +1,13 @@
+
+def cwd():
+    import os
+    path = os.getcwd()
+    print(f"The current working directory is  {path}")
+    for file in os.listdir(path):
+        print("The directory contains the following files:")
+        print(file)
+def run():
+    print("Processing....")
+    cwd()
+if __name__ ==  "__main__":
+    run()
