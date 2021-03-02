b09a55b535da38aea9d8870656adfb4ccf9532da
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-02 12:57:25 +0000

    to demonstrate how to write to a text file.

---
+++ data/files/txt/write.py
@@ -0,0 +1,28 @@
+def search(file_path):
+    print("Searching...")
+    sections = ""
+    books = "Books:\n"
+    with open(file_path) as file:
+        for line in file:
+            if line.startswith("Section"):
+                sections += line
+            else:
+                books += line
+
+    print("Done!")
+    return f"{sections}\n\n{books}"
+
+
+def save(file_path, data):
+    print("Saving...")
+    with open(file_path, "w") as file:
+        file.write(data)
+    print("Done!")
+
+
+def run():
+    data = search("books.txt")
+    save("exported_books.txt", data)
+
+if __name__ ==  "__main__":
+    run()
