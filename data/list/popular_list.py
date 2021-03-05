b261b59cdc4ac4cc0f7820a50946adac0045355c
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-05 11:05:46 +0000

    to demonstrate how to populate a list.

---
+++ data/list/popular_list.py
@@ -0,0 +1,22 @@
+def directions():
+    directions=["Move Forward", "Move Backward", "Turn Left" , "Turn Right"]
+    return directions
+def menu():
+    print("please select a direction")
+    path=directions()
+    for index in range(len(path)):
+        print(f"{index} : {path[index]}")
+    index = int(input())
+    return path[index]
+
+def run():
+    route = []
+    print("Working out escape route...")
+    for count in range(5):
+        route.append(menu())
+    print(f"Escape route: {route}")
+
+
+
+if __name__=="__main__":
+    run()
