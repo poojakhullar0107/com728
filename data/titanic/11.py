59484665d7bda914c74a26fec250fe7ae3389e81
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-06 16:48:48 +0000

    to display number of survivor per age group

--- data/titanic/11.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("titanic.csv")
-print(df['Age'].min())
-
-print(df.groupby('Age').count())
--- data/titanic/tinanic.py
+++ data/titanic/tinanic.py
@@ -1,18 +1,17 @@
-
-headings=[]
+headers=[]
 import csv
 
-
 #task1
-def load_data(filepath):
+
+def load_data():
     records=0
-    print("loading Data....")
-    with open(filepath) as file:
-        csv_reader=csv.reader(file)
-        headings=next(csv_reader)
+    with open("titanic.csv") as file:
+        csv_reader = csv.reader(file)
+        headers=next(csv_reader)
+        print("loading Data....")
         for values in csv_reader:
             records+=1
-    print (records)
+    print (f"Total number of passsengers are : {records}")
 
 #task2
 
@@ -22,6 +21,7 @@ def display_menu():
     print("[2] Display the number of passengers that survived")
     print("[3] Display the number of passengers per gender")
     print("[4] Display the number of passengers per age group")
+    print("[5] Display the number of survivors per age group")
     selected_option=int(input())
     print(f"you have selected option {selected_option}")
     return selected_option
@@ -33,7 +33,7 @@ def display_passengers_name():
     with open("titanic.csv") as file:
         csv_reader = csv.DictReader(file)
         for values in csv_reader:
-            print(values['Name'],values['Age'])
+            print(values['Name'])
 
 #task 4
 
@@ -76,8 +76,31 @@ def display_passengers_per_age_group():
         print(f"Number of Adult passenger are : {adults}")
         print(f"Number of Elderly passengers are : {elderly}")
 
+#task 7
+def display_survivors_per_age_group():
+    with open("titanic.csv",) as file:
+        csv_reader = csv.DictReader(file)
+        children = adults = elderly = 0
+        children_survived=adults_survived= elderly_survived=0
+        for values in csv_reader:
+            if values['Age']< '18':
+                children += 1
+                if values['Survived']=='1':
+                    children_survived+=1
+            elif values['Age'] >= '18' and values['Age']< '65':
+                adults+=1
+                if values['Survived']=='1':
+                    adults_survived+=1
+            elif values['Age']>='65':
+                elderly += 1
+                if values['Survived']=='1':
+                    elderly_survived+=1
+        print(f"Number of Young passengers survived are :{children_survived}/{children}")
+        print(f"Number of Adult passenger survived are : {adults_survived}/{adults}")
+        print(f"Number of Elderly passengers survived are :{elderly_survived}/{elderly}")
+
 def run():
-    load_data("titanic.csv")
+    load_data()
     selected_option=display_menu()
     if selected_option==1:
         display_passengers_name()
@@ -87,6 +110,8 @@ def run():
         diplay_passenger_per_gender()
     elif selected_option==4:
         display_passengers_per_age_group()
+    elif selected_option==5:
+        display_survivors_per_age_group()
     else: print("You have entered a wrong choice....bye!!")
 if __name__=="__main__":
     run()
