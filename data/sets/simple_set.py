76f5a38b66d1029b007dc4d77e209011ea41861d
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-12 10:11:50 +0000

    to demonstrate a simple set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,12 @@
+def observed():
+    observations=set([ "Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"])
+    return(observations)
+
+def run():
+    a=set()
+    a=observed()
+    print (f"Final Set is {a}")
+
+
+if __name__=="__main__":
+    run()
