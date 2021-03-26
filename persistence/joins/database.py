8421a3c7a02c17bcde6eb8da860a08f817e5a2be
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-26 17:23:37 +0000

    Added code to retrieve data using an inner join

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -13,3 +13,28 @@ def display_products_with_stock_levels():
 
     db.close()
 
+def display_product_supplier():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = "SELECT product.name, supplier.name from"\
+        " product INNER JOIN supplier "\
+        "ON product.supplier_id=supplier.id"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    for record in records:
+        print(f"Product is: {record[0]} ;Supplier is : {record[1]}\n")
+
+    db.close()
+def  display_product_supplier_locations():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = "SELECT product.name, supplier.name, "\
+            "location.city,location.country from" \
+            " product INNER JOIN supplier " \
+            "ON product.supplier_id=supplier.id"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    for record in records:
+        print(f"Product is: {record[0]} ;Supplier is : {record[1]}\n")
+
+    db.close()
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -2,8 +2,10 @@ import database
 def menu():
     print("Please select one of the following options:")
     print("[1] Display stock levels")
-    choice = int(input())
-    print(f"Your selection is :  {choice}")
-    if choice == 1 :
+    print("[2] Display Supplier")
+    choice = int(input(" Your Choice : "))
+    if choice == 1:
         database.display_products_with_stock_levels()
+    if choice == 2:
+        database.display_product_supplier()
 menu()
