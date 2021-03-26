ce7344e1e75885b334f5148739eb574e5a3c9cbd
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-03-26 17:41:34 +0000

    Added code to retrieve data using multiple inner join

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -1,4 +1,8 @@
 import sqlite3
+product_name=0
+supplier_name=1
+location_city=2
+location_country=3
 def display_products_with_stock_levels():
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
@@ -28,13 +32,14 @@ def display_product_supplier():
 def  display_product_supplier_locations():
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
-    sql = "SELECT product.name, supplier.name, "\
-            "location.city,location.country from" \
-            " product INNER JOIN supplier " \
-            "ON product.supplier_id=supplier.id"
+    sql = "SELECT product.name, supplier.name,location.city,"\
+        "location.country from product "\
+        "INNER JOIN supplier ON product.supplier_id=supplier.id ,"\
+        "location ON supplier.supplier_id=location.id"
     cursor.execute(sql)
     records = cursor.fetchall()
     for record in records:
-        print(f"Product is: {record[0]} ;Supplier is : {record[1]}\n")
+        print(f"Product : {record[product_name]} ;Supplier  : {record[supplier_name]}, location  : {record[location_city]} , {record [location_country]}\n")
 
     db.close()
+
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -3,9 +3,12 @@ def menu():
     print("Please select one of the following options:")
     print("[1] Display stock levels")
     print("[2] Display Supplier")
+    print("[3] Display Supplier locations")
     choice = int(input(" Your Choice : "))
     if choice == 1:
         database.display_products_with_stock_levels()
-    if choice == 2:
+    elif choice == 2:
         database.display_product_supplier()
+    elif choice == 3:
+        database.display_product_supplier_locations()
 menu()
