ef36e2dc552b27d82e28dc4b99399b53fbb625bc
poojakhullar0107 <5khulp71@solent.ac.uk>
2021-05-07 10:35:54 +0100

    Added code to create a basic animation function.

---
+++ visual/animations/simple.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+def animate(frame):
+    global ax
+    ax.set_xlim(0,10)
+    ax.set_ylim(0,10)
+    ax.plot(frame, frame,'r-o')
+
+
+# your code here
+
+def run():
+    global fig
+    ani = animation.FuncAnimation(fig, animate, frames=10, interval=1000)
+    plt.show()
+run()
